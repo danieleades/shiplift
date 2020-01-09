9d77d6f98c969bd9fe45a536976e9569decc2a38
Daniel Eades <danieleades@hotmail.com>
2020-01-09 17:56:52 +0000

    saving progress

--- examples/containers.rs
+++ examples/containers.rs
@@ -4,7 +4,7 @@ use shiplift::Docker;
 async fn main() {
     env_logger::init();
     let docker = Docker::new();
-    match docker.containers().list(&Default::default()).await {
+    match docker.containers().list().await {
         Ok(containers) => {
             for c in containers {
                 println!("container -> {:#?}", c)
